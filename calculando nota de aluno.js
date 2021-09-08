b13a58b5b3242194b51dcfe5fec1859f9c2fe0a4
Edson-html-css-js <ed_ls@outlook.com>
2021-09-08 16:54:34 -0300

    logica-1

---
+++ calculando nota de aluno.js
@@ -0,0 +1,36 @@
+
+
+
+    
+// var aluno1 = 10;
+// var aluno2 = 10;
+// var aluno3 = 10;
+// var aluno4 = 10;
+// var aluno5 = 10;
+// var media = (aluno1, aluno2, aluno3, aluno4, aluno5) / 5;
+
+// console.log(media);
+
+// var ano = 2021;
+
+// console.log("A idade de Edson Lucas da SIlva é " + (ano - 1990) + " anos de idade !");
+// console.log("A idade de Gislaine pinheiro dos santos é " + (ano - 1987));
+
+// console.log("taboada a frente >>>>>>>>>>>>")
+
+//var numero = 5;
+
+function pulaLinha(peso, altura) {
+    console.log("");
+
+}
+
+function escreva(frase) {
+
+    console.log(frase);
+    pulaLinha();
+};
+
+escreva ("fffffffffdsssssswwwwwwwwww");
+escreva ("edsin lucas");
+escreva ("Boa tarde meus amigos! ")
